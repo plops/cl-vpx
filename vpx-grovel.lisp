44b1fb898b700d490489b7fc006ef5c052ca0c09
Martin Kielhorn <kielhorn.martin@gmail.com>
2016-01-26 11:35:29 +0100

    define more constants in err_t

--- vpx-grovel.lisp
+++ vpx-grovel.lisp
@@ -8,4 +8,12 @@
 
 (cenum vpx_codec_err_t
        ((:vpx-codec-ok "VPX_CODEC_OK"))
-       ((:vpx-codec-error "VPX_CODEC_ERROR")))
+       ((:vpx-codec-error "VPX_CODEC_ERROR"))
+       ((:vpx-codec-mem-error "VPX_CODEC_MEM_ERROR"))
+       ((:vpx-codec-abi-mismatch "VPX_CODEC_ABI_MISMATCH"))
+       ((:vpx-codec-incapable "VPX_CODEC_INCAPABLE"))
+       ((:vpx-codec-unsup-bitstream "VPX_CODEC_UNSUP_BITSTREAM"))
+       ((:vpx-codec-unsup-feature "VPX_CODEC_UNSUP_FEATURE"))
+       ((:vpx-codec-corrupt-frame "VPX_CODEC_CORRUPT_FRAME"))
+       ((:vpx-codec-invalid-param "VPX_CODEC_INVALID_PARAM"))
+       ((:vpx-codec-list-end "VPX_CODEC_LIST_END")))
